cf9b905e61c0dd7df3ce3347054e78217292791b
hieuSotatek <156876017+hieuSotatek@users.noreply.github.com>
2024-05-06 15:04:52 +0700

    Add files via upload

---
+++ ISAdMobAdapter.xcframework/ios-arm64/ISAdMobAdapter.framework/Headers/ISAdMobAdapter.h
@@ -0,0 +1,38 @@
+//
+//  ISAdMobAdapter.h
+//  ISAdMobAdapter
+//
+//  Copyright © 2023 ironSource Mobile Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <IronSource/ISBaseAdapter+Internal.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+static NSString * const AdMobAdapterVersion = @"4.3.54";
+static NSString * Githash = @"89c3ab3";
+
+//System Frameworks For AdMob Adapter
+
+@import AdSupport;
+@import AudioToolbox;
+@import AVFoundation;
+@import CFNetwork;
+@import CoreGraphics;
+@import CoreMedia;
+@import CoreTelephony;
+@import CoreVideo;
+@import JavaScriptCore;
+@import MediaPlayer;
+@import MessageUI;
+@import MobileCoreServices;
+@import QuartzCore;
+@import SafariServices;
+@import Security;
+@import StoreKit;
+@import SystemConfiguration;
+@import WebKit;
+
+@interface ISAdMobAdapter : ISBaseAdapter
+
+@end
